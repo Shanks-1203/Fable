fa8e3ec1d946c43f7fa4131829d4f03b9359ce1a
Surya K <suryasingam363@gmail.com>
2025-02-05 11:47:41 +0530

    initial commit

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className='w-full flex justify-between items-center text-[#8967B3] bg-[#EEEDED] z-10 px-8 h-[8vh] fixed top-0'>
         <div className='flex justify-start items-center py-2 gap-1'>
             <Image src={logo} alt='logo' className='w-[3.5rem]'/>
-            <p className='text-2xl font-bold'>fable.</p>
+            <p className='text-2xl font-bold'>Fable</p>
         </div>
         <Link href='/profile'><FaRegUser className='cursor-pointer text-2xl'/></Link>
     </div>
